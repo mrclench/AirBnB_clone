977f2e4d4a4365d85c57d9b951696e6704c4c4a6
mrclench <charlesnnnortey@gmail.com>
2024-03-21 09:16:32 +0000

    tests run

--- models/base_model.py
+++ models/base_model.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python3
 import uuid
+from datetime import datetime
 
 """Creating BaseModel class"""
 
 class BaseModel:
     """This is the base model"""
-    def __init__(self, id=None):
+    def __init__(self):
         """Initializing base class"""
         self.id = uuid.uuid4()
-
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+        self.name = ""
+        self.my_number = 0
 
     def __str__(self):
-        return("[{}] ({})".format(self.__class__.__name__, self.id))
+        return("[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__))
+
